4cc4ec3463a9193d1a8f3de10812db65fe1670f2
Kristoffer Klintberg <krstffr@gmail.com>
2014-11-11 15:00:50 +0100

    Added classes to submit buttons for styling.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A form creator for Meteor.js.",
-  version: "0.1.3",
+  version: "0.1.4",
   git: "https://github.com/krstffr/meteor-form-creator",
   name: "krstffr:form-creator"
 });
