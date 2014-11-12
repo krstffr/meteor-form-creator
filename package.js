fcaf67f36685af91c1cb1215e2ee0b43d62e0057
Kristoffer Klintberg <krstffr@gmail.com>
2014-11-12 19:21:00 +0100

    Fixed bug where forms would be added as arrays (of nested forms!) instead of the actual forms.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A form creator for Meteor.js.",
-  version: "0.1.5",
+  version: "0.1.6",
   git: "https://github.com/krstffr/meteor-form-creator",
   name: "krstffr:form-creator"
 });
